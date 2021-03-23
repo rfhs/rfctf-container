90a1ce9ffaad62ce230935dab50c4fd3fd99822d
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2021-03-23 14:19:27 -0400

    container which has the clients for wifi, and will eventually have the servers for sdr

---
+++ infrastructure_containers/client_server/make_modules.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+##make the gentoo portage module
+rm -rf /dev/shm/portage/rootfs/var/db/repos/gentoo
+mkdir -p /dev/shm/portage/rootfs/var/db/repos/gentoo
+rsync -aEXu --delete /var/db/repos/gentoo/* /dev/shm/portage/rootfs/var/db/repos/gentoo
+## add the pentoo overlay
+mkdir -p /dev/shm/portage/rootfs/var/db/repos/pentoo/
+rsync -aEXu --progress --delete /var/db/repos/pentoo/ /dev/shm/portage/rootfs/var/db/repos/pentoo/
+#fix the perms
+chown root.root /dev/shm/portage/rootfs/var/db/repos
+chown root.root /dev/shm/portage/rootfs/var/db
+chown root.root /dev/shm/portage/rootfs/var
+chown root.root /dev/shm/portage/rootfs
+chown root.root /dev/shm/portage
+chown portage.portage -R /dev/shm/portage/rootfs/var/db/repos/pentoo
+chown portage.portage -R /dev/shm/portage/rootfs/var/db/repos/gentoo
+# make the unified tarball
+tar cJf repos.tar.xz -C /dev/shm/portage/rootfs/ .
