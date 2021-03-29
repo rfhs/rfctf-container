6b4b1ec053e74bc307eca7634b943da8e760fbd8
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2021-03-28 21:41:02 -0400

    cleanup client/server container a little bit

--- infrastructure_containers/client_server/build.sh
+++ infrastructure_containers/client_server/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-VERS="0.1"
-docker build . --pull -t rfhs/client_server:${VERS}
+VERS="0.2"
+docker build . -t rfhs/client_server:${VERS}
 docker tag rfhs/client_server:${VERS} rfhs/client_server:latest
 #docker push rfhs/client_server
 #docker push rfhs/client_server:latest
