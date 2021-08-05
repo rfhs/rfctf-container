c702727ded6031b73e72bfa3cab92f16e6e725fe
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2021-08-05 10:14:30 -0400

    add image-prep.sh so I stop skipping steps

---
+++ misc/image-prep.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# This script is used before creating the game image to clean up the master
+
+# stop all running docker containers
+if [ -n "$(docker ps -a -q)" ]; then
+  docker stop $(docker ps -a -q)
+fi
+# remove any stopped containers which weren't removed already
+if [ -n "$(docker ps -a -q)" ]; then
+  docker rm $(docker ps -a -q)
+fi
+
+# pull latest images for everything
+for image in $(docker image ls | grep '^rfhs' | awk '{print $1}'); do
+  docker pull "${image}"
+done
+# cleanup untagged docker images
+docker rmi $(docker images | grep "<none>" | awk '{print $3}')
+
+# cleanup unneeded gentoo files leftover from upgrading
+rm -rf "$(portageq envvar DISTDIR)"/*
+rm -rf "$(portageq envvar PKGDIR)"/*
+
+# clean cloud init
+cloud-init clean
