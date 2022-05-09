d6776756065f6a24fd6b8eca572d86924e4b2fe4
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2022-05-09 16:09:07 -0400

    break apart client and sdr containers

---
+++ infrastructure_containers/client_server/build-rfctf-sdr.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+VERS="1.0"
+DISTRO="rfctf-sdr"
+docker build . --pull -f "Dockerfile.${DISTRO}" -t rfhs/${DISTRO}:${VERS}
+docker tag rfhs/${DISTRO}:${VERS} rfhs/${DISTRO}:latest
+if [ "$(hostname)" = "Nu" ] ; then
+  docker push rfhs/${DISTRO}
+  docker push rfhs/${DISTRO}:latest
+fi
