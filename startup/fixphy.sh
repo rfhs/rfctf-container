29410ae078a8d8a8ba26f3cce8013f14af43f9e3
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2022-05-10 16:21:19 -0400

    retab, prepping for some fixes

--- startup/fixphy.sh
+++ startup/fixphy.sh
@@ -7,91 +7,91 @@ FIX_PHY="${FIX_PHY:-}"
 DEBUG="${DEBUG:-}"
 
 if [ -n "${DEBUG}" ]; then
-        set -x
+  set -x
 fi
 
 findFreeInterface() {
-        if [ -z "${1}" ]; then
-                printf "findFreeInterface needs a target mode.\n"
-                exit 1
+  if [ -z "${1}" ]; then
+    printf "findFreeInterface needs a target mode.\n"
+    exit 1
+  fi
+  if [ "${1}" != "monitor" ] && [ "${1}" != "station" ]; then
+    printf "findFreeInterface only supports monitor and station for target mode.\n"
+    exit 1
+  fi
+  target_mode="${1}"
+  if [ "$target_mode" = "monitor" ]; then
+    target_suffix="mon"
+    target_type="803"
+  else
+    target_suffix=""
+    target_type="1"
+  fi
+  for i in $(seq 0 100); do
+    if [ "$i" = "100" ]; then
+      printf "\nUnable to find a free name between wlan0 and wlan99, you are on your own from here.\n"
+      return 1
+    fi
+    if [ "$DEBUG" = "1" ]; then
+      printf "\nChecking candidate wlan${i}\n"
+    fi
+    if [ ! -e /sys/class/net/wlan${i} ]; then
+      if [ "$DEBUG" = "1" ]; then
+        printf "\nCandidate wlan${i} is not in use\n"
+      fi
+      if [ ! -e /sys/class/net/wlan${i}mon ]; then
+        if [ "$DEBUG" = "1" ]; then
+          printf "\nCandidate wlan${i} and wlan${i}mon are both clear, creating wlan${i}${target_suffix}\n"
         fi
-        if [ "${1}" != "monitor" ] && [ "${1}" != "station" ]; then
-                printf "findFreeInterface only supports monitor and station for target mode.\n"
-                exit 1
-        fi
-        target_mode="${1}"
-        if [ "$target_mode" = "monitor" ]; then
-                target_suffix="mon"
-                target_type="803"
+        #IW_ERROR="$(iw phy ${PHYDEV} interface add wlan${i}${target_suffix} type ${target_mode} 2>&1)"
+        IW_ERROR="$(iw phy ${PHYDEV} interface add wlan${i}${target_suffix} type __ap 2>&1)"
+        if [ -z "${IW_ERROR}" ]; then
+          if [ -d /sys/class/ieee80211/${PHYDEV}/device/net ]; then
+            for j in $(ls /sys/class/ieee80211/${PHYDEV}/device/net/); do
+              if [ "$(cat /sys/class/ieee80211/${PHYDEV}/device/net/${j}/type)" = "${target_type}" ]; then
+                #here is where we catch udev renaming our interface
+                k=${j#wlan}
+                i=${k%mon}
+              fi
+            done
+          else
+            printf "Unable to create wlan${i}${target_suffix} and no error received.\n"
+            return 1
+          fi
+          printf "\n(mac80211 ${target_mode} mode vif enabled on [${PHYDEV}]wlan${i}${target_suffix}\n"
+          unset IW_ERROR
+          break
         else
-                target_suffix=""
-                target_type="1"
+          printf "\n\n ERROR adding ${target_mode} mode interface: ${IW_ERROR}\n"
+          break
         fi
-        for i in $(seq 0 100); do
-                if [ "$i" = "100" ]; then
-                        printf "\nUnable to find a free name between wlan0 and wlan99, you are on your own from here.\n"
-                        return 1
-                fi
-                if [ "$DEBUG" = "1" ]; then
-                        printf "\nChecking candidate wlan${i}\n"
-                fi
-                if [ ! -e /sys/class/net/wlan${i} ]; then
-                        if [ "$DEBUG" = "1" ]; then
-                                printf "\nCandidate wlan${i} is not in use\n"
-                        fi
-                        if [ ! -e /sys/class/net/wlan${i}mon ]; then
-                                if [ "$DEBUG" = "1" ]; then
-                                        printf "\nCandidate wlan${i} and wlan${i}mon are both clear, creating wlan${i}${target_suffix}\n"
-                                fi
-                                #IW_ERROR="$(iw phy ${PHYDEV} interface add wlan${i}${target_suffix} type ${target_mode} 2>&1)"
-                                IW_ERROR="$(iw phy ${PHYDEV} interface add wlan${i}${target_suffix} type __ap 2>&1)"
-                                if [ -z "${IW_ERROR}" ]; then
-                                        if [ -d /sys/class/ieee80211/${PHYDEV}/device/net ]; then
-                                                for j in $(ls /sys/class/ieee80211/${PHYDEV}/device/net/); do
-                                                        if [ "$(cat /sys/class/ieee80211/${PHYDEV}/device/net/${j}/type)" = "${target_type}" ]; then
-                                                                #here is where we catch udev renaming our interface
-                                                                k=${j#wlan}
-                                                                i=${k%mon}
-                                                        fi
-                                                done
-                                        else
-                                                printf "Unable to create wlan${i}${target_suffix} and no error received.\n"
-                                                return 1
-                                        fi
-                                        printf "\n(mac80211 ${target_mode} mode vif enabled on [${PHYDEV}]wlan${i}${target_suffix}\n"
-                                        unset IW_ERROR
-                                        break
-                                else
-                                        printf "\n\n ERROR adding ${target_mode} mode interface: ${IW_ERROR}\n"
-                                        break
-                                fi
-                        else
-                                if [ "$DEBUG" = "1" ]; then
-                                        printf "\nCandidate wlan${i} does not exist, but wlan${i}mon does, skipping...\n"
-                                fi
-                        fi
-                else
-                        if [ "$DEBUG" = "1" ]; then
-                                printf "\nCandidate wlan${i} is in use already.\n"
-                        fi
-                fi
-        done
+      else
+        if [ "$DEBUG" = "1" ]; then
+          printf "\nCandidate wlan${i} does not exist, but wlan${i}mon does, skipping...\n"
+        fi
+      fi
+    else
+      if [ "$DEBUG" = "1" ]; then
+        printf "\nCandidate wlan${i} is in use already.\n"
+      fi
+    fi
+  done
 }
 
 handleLostPhys() {
-        MISSING_INTERFACE=""
-        if [ -d /sys/class/ieee80211 ]; then
-                for i in $(ls /sys/class/ieee80211/); do
-                        if [ ! -d /sys/class/ieee80211/${i}/device/net ]; then
-                                MISSING_INTERFACE="${i}"
-                                PHYDEV=${MISSING_INTERFACE}
-                                echo "Found missing interface ${PHYDEV}"
-                                if [ -n "${FIX_PHY}" ]; then
-                                        findFreeInterface station
-                                fi
-                        fi
-                done
+  MISSING_INTERFACE=""
+  if [ -d /sys/class/ieee80211 ]; then
+    for i in $(ls /sys/class/ieee80211/); do
+      if [ ! -d /sys/class/ieee80211/${i}/device/net ]; then
+        MISSING_INTERFACE="${i}"
+        PHYDEV=${MISSING_INTERFACE}
+        echo "Found missing interface ${PHYDEV}"
+        if [ -n "${FIX_PHY}" ]; then
+          findFreeInterface station
         fi
+      fi
+    done
+  fi
 }
 
 handleLostPhys
--- startup/namespace_fixup.sh
+++ startup/namespace_fixup.sh
@@ -3,50 +3,50 @@
 set -eu -o pipefail
 
 if [ -n "$(command -v id 2> /dev/null)" ]; then
-	USERID="$(id -u 2> /dev/null)"
+  USERID="$(id -u 2> /dev/null)"
 fi
 
 if [ -z "${USERID}" ] && [ -n "$(id -ru)" ]; then
-	USERID="$(id -ru)"
+  USERID="$(id -ru)"
 fi
 
 if [ -n "${USERID}" ] && [ "${USERID}" != "0" ]; then
-	printf "Run it as root\n" ; exit 1;
+  printf "Run it as root\n" ; exit 1;
 elif [ -z "${USERID}" ]; then
-	printf "Unable to determine user id, permission errors may occur.\n"
+  printf "Unable to determine user id, permission errors may occur.\n"
 fi
 
 #CONTAINER_NAME="${1:-}"
 
 if [[ $EUID -ne 0 ]]; then
-   echo -e "You *did* plan on mutating namespace configurations, right...?\n"
+  echo -e "You *did* plan on mutating namespace configurations, right...?\n"
 
-   echo "This script must be run as root" 
-   exit 1
+  echo "This script must be run as root" 
+  exit 1
 fi
 
 function create_ns_link {
-        local PID=$(docker inspect -f '{{.State.Pid}}' ${CONTAINER_NAME})
+  local PID=$(docker inspect -f '{{.State.Pid}}' ${CONTAINER_NAME})
 
-        if [ -z "${PID}" ]; then
-                echo "Unable to identify process id for ${CONTAINER_NAME}, skipping."
-                exit
-        fi
-        mkdir -p /run/netns/
-        touch "/run/netns/${CONTAINER_NAME}"
-        echo "Mapping namespaces of process id ${PID} for ${CONTAINER_NAME} to namespace name ${CONTAINER_NAME}"
-        mount -o bind "/proc/${PID}/ns/net" "/run/netns/${CONTAINER_NAME}"
+  if [ -z "${PID}" ]; then
+    echo "Unable to identify process id for ${CONTAINER_NAME}, skipping."
+    exit
+  fi
+  mkdir -p /run/netns/
+  touch "/run/netns/${CONTAINER_NAME}"
+  echo "Mapping namespaces of process id ${PID} for ${CONTAINER_NAME} to namespace name ${CONTAINER_NAME}"
+  mount -o bind "/proc/${PID}/ns/net" "/run/netns/${CONTAINER_NAME}"
 }
 
 if [ -z "$*" ]; then
-#       CONTAINERS="openwrt wctf-client contestant"
-        CONTAINERS="$(docker ps --format '{{ .Names }}')"
+  #       CONTAINERS="openwrt wctf-client contestant"
+  CONTAINERS="$(docker ps --format '{{ .Names }}')"
 else
-        CONTAINERS="$*"
+  CONTAINERS="$*"
 fi
 
 #for CONTAINER in "openwrt" "wctf-client" "contestant"; do
 for CONTAINER in ${CONTAINERS}; do
-        CONTAINER_NAME="${CONTAINER}"
-        create_ns_link
+  CONTAINER_NAME="${CONTAINER}"
+  create_ns_link
 done
