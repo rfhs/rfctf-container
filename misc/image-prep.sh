e51f29f6df99816e60c7d8c4a2c5f15c15a0039e
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2022-09-15 21:40:31 -0400

    minor updates
    Clean docker while updating to avoid using double the disk space.
    Remove new location of shared_persistent_storage too

--- misc/image-prep.sh
+++ misc/image-prep.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # This script is used before creating the game image to clean up the master
 
+cleanup_docker() {
+  # cleanup untagged docker images
+  if [ -n "$(docker images | grep "<none>" | awk '{print $3}')" ]; then
+    docker rmi $(docker images | grep "<none>" | awk '{print $3}')
+  fi
+}
+
 # stop all running docker containers
 if [ -n "$(docker ps -a -q)" ]; then
   docker stop $(docker ps -a -q)
@@ -13,11 +20,8 @@ fi
 # pull latest images for everything
 for image in $(docker image ls | grep '^rfhs' | awk '{print $1}'); do
   docker pull "${image}"
+  cleanup_docker
 done
-# cleanup untagged docker images
-if [ -n "$(docker images | grep "<none>" | awk '{print $3}')" ]; then
-  docker rmi $(docker images | grep "<none>" | awk '{print $3}')
-fi
 
 # cleanup unneeded gentoo files leftover from upgrading
 if [ -x "$(command -v portageq 2>&1)" ]; then
@@ -29,6 +33,9 @@ fi
 # ensure shared-persistent_storage is empty
 if [ -d '/var/wctf/shared_persistent_storage/*' ]; then
   rm -rf /var/wctf/shared_persistent_storage/*
+fi
+if [ -d '/var/cache/rfhs-rfctf/shared_persistent_storage/*' ]; then
+  rm -rf /var/cache/rfhs-rfctf/shared_persistent_storage/*
 fi
 
 #wipe all the container logs
