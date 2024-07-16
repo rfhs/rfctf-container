rfhs/rfctf-container

commit ebdae57a96338b3cd76638d28fcba366dac82af0

enable X11 forwarding in contestant containers
lower indent in blackarch Dockerfile as well
Closes: https://github.com/rfhs/rfctf-container/issues/18

contestant_containers/Dockerfile.blackarch
@@ -1,26 +1,27 @@
 FROM docker.io/blackarchlinux/blackarch:latest
 
 RUN \
-    # This is required or keys fail
-    pacman-key --init && \
-    pacman -Syu --noconfirm archlinux-keyring && \
-    gpgconf --kill all && \
-    # Install packages, missing fldigi, qsstv, wsjtx, gr-lora_sdr, nrsc5
-    pacman -Syu --noconfirm python-pip openssh vim tmux screen supervisor iw man mlocate pciutils less bash-completion novnc \
-    xorg-server-xvfb x11vnc xfce4 xfce4-goodies xfce4-power-manager blackarch-config-xfce blackarch-menus blackarch-wallpaper \
-    blackarch-config-cursor blackarch-config-icons blackarch-config-zsh ttf-liberation hostapd freeradius wpa_supplicant bully \
-    wireshark-qt wireshark-cli aircrack-ng mdk3 mdk4 asleap hostapd-wpe wifite pixiewps reaver hashcat hashcat-utils hcxtools \
-    hcxdumptool wifi-honey tcpdump nmap gnuradio gqrx gnuradio-osmosdr urh firefox && \
-    # Uninstall broken package
-    pacman -R --noconfirm xfce4-power-manager && \
-    # Clean up environment
-    rm -r /etc/hostapd-wpe/certs/* && \
-    # Point wallpaper to the right files
-    sed -i 's/backgrounds\/blackarch.png/blackarch\/wallpaper.png/g' /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml && \
-    # Copy BlackArch configs
-    cp -r /etc/skel/. /root/. && \
-    # clear pacman's cache
-    pacman -Scc <<< Y <<< Y
+  # This is required or keys fail
+  pacman-key --init && \
+  pacman -Syu --noconfirm archlinux-keyring && \
+  gpgconf --kill all && \
+  # Install packages, missing fldigi, qsstv, wsjtx, gr-lora_sdr, nrsc5
+  pacman -Syu --noconfirm python-pip openssh vim tmux screen supervisor iw man mlocate pciutils less bash-completion novnc \
+  xorg-server-xvfb x11vnc xfce4 xfce4-goodies xfce4-power-manager blackarch-config-xfce blackarch-menus blackarch-wallpaper \
+  blackarch-config-cursor blackarch-config-icons blackarch-config-zsh ttf-liberation hostapd freeradius wpa_supplicant bully \
+  wireshark-qt wireshark-cli aircrack-ng mdk3 mdk4 asleap hostapd-wpe wifite pixiewps reaver hashcat hashcat-utils hcxtools \
+  hcxdumptool wifi-honey tcpdump nmap gnuradio gqrx gnuradio-osmosdr urh firefox && \
+  # Uninstall broken package
+  pacman -R --noconfirm xfce4-power-manager && \
+  # Clean up environment
+  rm -r /etc/hostapd-wpe/certs/* && \
+  # Point wallpaper to the right files
+  sed -i 's/backgrounds\/blackarch.png/blackarch\/wallpaper.png/g' /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml && \
+  # Copy BlackArch configs
+  cp -r /etc/skel/. /root/. && \
+  # clear pacman's cache
+  pacman -Scc <<< Y <<< Y && \
+  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 
 # Expose needed ports
 EXPOSE 22/tcp

contestant_containers/Dockerfile.kali
@@ -22,7 +22,8 @@ RUN DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true apt-get upda
   make clean && \
   cd /etc/hostapd-wpe/certs && \
   make clean && \
-  touch ~/.hushlogin
+  touch ~/.hushlogin && \
+  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 
 # Expose needed ports
 EXPOSE 22/tcp

contestant_containers/Dockerfile.parrot
@@ -14,7 +14,8 @@ RUN \
   cd /etc/freeradius-wpe/3.0/certs && \
   make clean && \
   cd /etc/hostapd-mana/certs && \
-  rm ca.pem csr.csr dhparam.pem server.key server.pem
+  rm ca.pem csr.csr dhparam.pem server.key server.pem && \
+  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 
 # Expose needed ports
 EXPOSE 22/tcp

contestant_containers/Dockerfile.pentoo
@@ -35,7 +35,8 @@ RUN sed -i 's#-livecd#-drivers  -vnc opencl#' /etc/portage/make.conf && \
   # also depclean bdeps and deps
   emerge --depclean --with-bdeps=n --jobs=$(nproc) && \
   # rm all the files we don't want
-  rm -rf /var/cache/{binpkgs,distfiles}/* /var/db/repos/* /usr/share/doc/*
+  rm -rf /var/cache/{binpkgs,distfiles}/* /var/db/repos/* /usr/share/doc/* && \
+  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 RUN \
   # configure the gui to be nicer
   # magic to autohide panel 2

contestant_containers/build-blackarch
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-VERS="0.29"
+VERS="0.30"
 DISTRO="blackarch"
 if [ -n "$(docker ps --filter name="rfhs-${DISTRO}-ci" --format '{{ .ID }}' )" ]; then
 	echo "Found existing rfhs-${DISTRO}-ci container... QUITTING"

contestant_containers/build-kali
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-VERS="0.29"
+VERS="0.30"
 DISTRO="kali"
 if [ -n "$(docker ps --filter name="rfhs-${DISTRO}-ci" --format '{{ .ID }}' )" ]; then
 	echo "Found existing rfhs-${DISTRO}-ci container... QUITTING"

contestant_containers/build-parrot
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-VERS="0.30"
+VERS="0.31"
 DISTRO="parrot"
 if [ -n "$(docker ps --filter name="rfhs-${DISTRO}-ci" --format '{{ .ID }}' )" ]; then
 	echo "Found existing rfhs-${DISTRO}-ci container... QUITTING"

contestant_containers/build-pentoo
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-VERS="0.29"
+VERS="0.30"
 DISTRO="pentoo"
 #./make_gentoo_cache.sh
 if [ -n "$(docker ps --filter name="rfhs-${DISTRO}-ci" --format '{{ .ID }}' )" ]; then

commit f620b306990fc8659341e99958bba32ca7674ff0

actually edit sshd_config
Fixes: ebdae57a96338b3cd76638d28fcba366dac82af0

contestant_containers/Dockerfile.blackarch
@@ -21,7 +21,7 @@ RUN \
   cp -r /etc/skel/. /root/. && \
   # clear pacman's cache
   pacman -Scc <<< Y <<< Y && \
-  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
+  sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 
 # Expose needed ports
 EXPOSE 22/tcp

contestant_containers/Dockerfile.kali
@@ -23,7 +23,7 @@ RUN DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true apt-get upda
   cd /etc/hostapd-wpe/certs && \
   make clean && \
   touch ~/.hushlogin && \
-  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
+  sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 
 # Expose needed ports
 EXPOSE 22/tcp

contestant_containers/Dockerfile.parrot
@@ -15,7 +15,7 @@ RUN \
   make clean && \
   cd /etc/hostapd-mana/certs && \
   rm ca.pem csr.csr dhparam.pem server.key server.pem && \
-  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
+  sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 
 # Expose needed ports
 EXPOSE 22/tcp

contestant_containers/Dockerfile.pentoo
@@ -36,7 +36,7 @@ RUN sed -i 's#-livecd#-drivers  -vnc opencl#' /etc/portage/make.conf && \
   emerge --depclean --with-bdeps=n --jobs=$(nproc) && \
   # rm all the files we don't want
   rm -rf /var/cache/{binpkgs,distfiles}/* /var/db/repos/* /usr/share/doc/* && \
-  sed 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
+  sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
 RUN \
   # configure the gui to be nicer
   # magic to autohide panel 2