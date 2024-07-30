5600eec79f80c520cb79be0f5e924a67ccdacd0d
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2024-07-30 13:32:04 -0400

    remove unneeded hack, fix ci

--- infrastructure_containers/build-rfctf-sdr.sh
+++ infrastructure_containers/build-rfctf-sdr.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-VERS="1.0"
+VERS="1.1"
 DISTRO="rfctf-sdr"
 docker build . --pull -f "Dockerfile.${DISTRO}" -t rfhs/${DISTRO}:${VERS}
 docker tag rfhs/${DISTRO}:${VERS} rfhs/${DISTRO}:latest
-if docker run -it --rm --name "rfhs-${DISTRO}-ci" rfhs/${DISTRO} ./challengectl.py --test --flagfile flags.txt.ci --devicefile devices.txt.ci; then
+if docker run --rm --name "rfhs-${DISTRO}-ci" rfhs/${DISTRO} ./challengectl.py --test --flagfile flags.txt.ci --devicefile devices.txt.ci; then
   if [ "$(hostname)" = "Nu" ] ; then
     docker push rfhs/${DISTRO}
     docker push rfhs/${DISTRO}:${VERS}
