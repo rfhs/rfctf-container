98a800fa6cc1e9b3a177e7dc89d1590e4db7c142
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2024-07-30 12:42:58 -0400

    sync gentoo cache script

--- contestant_containers/make_gentoo_cache.sh
+++ contestant_containers/make_gentoo_cache.sh
@@ -10,14 +10,14 @@ rsync -aEXu --delete /var/db/repos/gentoo/* /dev/shm/portage/rootfs/var/db/repos
 mkdir -p /dev/shm/portage/rootfs/var/db/repos/pentoo/
 rsync -aEXu --progress --delete /var/db/repos/pentoo/ /dev/shm/portage/rootfs/var/db/repos/pentoo/
 #fix the perms
-chown root.root /dev/shm/portage/rootfs/var/db/repos
-chown root.root /dev/shm/portage/rootfs/var/db
-chown root.root /dev/shm/portage/rootfs/var
-chown root.root /dev/shm/portage/rootfs
-chown root.root /dev/shm/portage
-chown portage.portage -R /dev/shm/portage/rootfs/var/db/repos/pentoo
-chown portage.portage -R /dev/shm/portage/rootfs/var/db/repos/gentoo
+chown root:root /dev/shm/portage/rootfs/var/db/repos
+chown root:root /dev/shm/portage/rootfs/var/db
+chown root:root /dev/shm/portage/rootfs/var
+chown root:root /dev/shm/portage/rootfs
+chown root:root /dev/shm/portage
+chown portage:portage -R /dev/shm/portage/rootfs/var/db/repos/pentoo
+chown portage:portage -R /dev/shm/portage/rootfs/var/db/repos/gentoo
 # make the unified tarball
 tar cJf portage_and_overlay.tar.xz -C /dev/shm/portage/rootfs/ .
 #don't waste RAM forever
-rm -rf /dev/shm/portage/rootfs/var/db/repos/gentoo
+rm -rf /dev/shm/portage/rootfs
