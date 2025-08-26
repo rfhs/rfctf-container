6fdddf545c3a4ec9934a6943d785f4c95558773d
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2025-08-25 21:15:46 -0400

    sdr: minor updates from testing fhardened bug (not fixed yet)

--- infrastructure_containers/build-rfctf-sdr.sh
+++ infrastructure_containers/build-rfctf-sdr.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
-VERS="1.1"
+VERS="1.2"
 DISTRO="rfctf-sdr"
 docker pull docker.io/pentoolinux/pentoo-core
 docker build --no-cache . --progress=plain -f "Dockerfile.${DISTRO}" -t rfhs/${DISTRO}:${VERS}
 docker tag rfhs/${DISTRO}:${VERS} rfhs/${DISTRO}:latest
-if docker run --rm --name "rfhs-${DISTRO}-ci" rfhs/${DISTRO} ./challengectl.py --test --flagfile flags.txt.ci --devicefile devices.txt.ci; then
-  if [ "$(hostname)" = "Nu" ] ; then
+if docker run --rm --network none --name "rfhs-${DISTRO}-ci" rfhs/${DISTRO} ./challengectl.py --test --flagfile flags.txt.ci --devicefile devices.txt.ci; then
+  if [ "$(hostname)" = "Nu" ] || [ "$(hostname)" = "naga" ]; then
     docker push rfhs/${DISTRO}
     docker push rfhs/${DISTRO}:${VERS}
     docker push rfhs/${DISTRO}:latest
